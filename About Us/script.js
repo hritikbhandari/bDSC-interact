ea876df2b0bda27f5b6a636bae56b71eed1925bf
Hritik Bhandari <bhandari.hritik@gmail.com>
2020-02-18 01:47:34 +0530

    Add files via upload

---
+++ About Us/script.js
@@ -0,0 +1,11 @@
+// $(document).$('#btnsvg').click(function () {
+//     alert("hnj")
+// });
+
+// $(document).ready(function() {
+//     $('#btnsvg').click(function() {
+//         $('svg rect.cls-2').attr("begin",  2);
+//         $('svg rect.cls-2').attr("begin",  2);
+//     });
+// });
+
